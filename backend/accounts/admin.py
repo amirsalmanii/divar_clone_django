74ac618243668f3bb6b064d3b0e9c394e59e2d0b
amirSalmani <salmaniamir13@gmail.com>
2022-08-19 23:04:21 +0430

    admin and model completed

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User as CustomUser
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=250, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
