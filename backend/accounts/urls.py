0fb431c2fe1ddd7d21c4b72fca130457173ae35d
amirSalmani <salmaniamir13@gmail.com>
2022-08-23 16:35:56 +0430

    added app_name

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index, login_view
 
+app_name = 'accounts'
 urlpatterns = [
   path('', index, name='index'),
   path('login/', login_view, name='login'),
