ffe371f221bcbc38df16ef4eece8d1cd60375f0a
amirSalmani <salmaniamir13@gmail.com>
2022-08-23 11:19:45 +0430

    add login view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'base.html', {})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            email, password = data['email'], data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return render(request, 'base.html', {})
+            return HttpResponse('user not fund')
+        return HttpResponse(f'not valid data-- {form.errors}')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
